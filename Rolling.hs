0d0d01705a834d52dacb864d4ae8a30e70450775
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-11 23:09:25 +0100

    naive chunksplitting

--- Rolling.hs
+++ Rolling.hs
@@ -3,7 +3,8 @@ module Rolling where
 
 import Data.Word
 import Data.Bits
-import Test.QuickCheck
+import Data.List
+import Test.QuickCheck hiding ((.&.))
 
 prime :: Word64
 prime = 16411
@@ -15,7 +16,8 @@ mk :: Word64 -> ModPrime
 mk x = P $ x `rem` prime
 
 instance Num ModPrime where
-  fromInteger x = mk $ fromInteger x
+  fromInteger x | x >= 0 = mk $ fromInteger x
+                | otherwise = negate . mk . fromInteger $ -x
   P x + P y = mk $ x + y
   P x * P y = mk $ x * y
   negate (P x) = mk $ prime - x
@@ -35,6 +37,9 @@ window = 128
 size :: ModPrime
 size = 256
 
+mask :: Word64
+mask = 1 `shiftL` 13 - 1
+
 droppedMultiplier :: ModPrime
 droppedMultiplier = size ^ (window - 1)
 
@@ -42,10 +47,16 @@ roll1 :: ModPrime -> (ModPrime, ModPrime) -> ModPrime
 roll1 h (n,o) = size * (h - droppedMultiplier * o) + n
 
 roll :: [ModPrime] -> [ModPrime]
-roll xs = scanl roll1 0 (zip xs (replicate window 0 ++ xs))
+roll xs = tail $ scanl roll1 0 withOld
+    where withOld = zip xs (replicate window 0 ++ xs)
 
 windowOrBigger :: Gen [ModPrime]
 windowOrBigger = sized $ \n -> do k <- choose (window, window + n)
                                   vector k
 
 prop_roll = forAll windowOrBigger $ \x -> last (roll x) == last (roll (drop (length x - window) x))
+
+rollsplit :: [ModPrime] -> [Int]
+rollsplit xs = diff $ 0 : indices ++ [length xs]
+    where indices = findIndices (\(P x) -> x .&. mask == mask) $ roll xs
+          diff = zipWith (-) =<< tail -- from the dungeons
