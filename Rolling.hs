27d4a89da4569dfc6c9a700fab017aae52093017
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-11 03:44:05 +0100

    rolling hash implementation

--- Rolling.hs
+++ Rolling.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Rolling where
 
 import Data.Word
@@ -7,23 +8,44 @@ import Test.QuickCheck
 prime :: Word64
 prime = 16411
 
+newtype ModPrime = P Word64
+  deriving (Show, Eq, Ord, Enum, Real, Integral)
+
+mk :: Word64 -> ModPrime
+mk x = P $ x `rem` prime
+
+instance Num ModPrime where
+  fromInteger x = mk $ fromInteger x
+  P x + P y = mk $ x + y
+  P x * P y = mk $ x * y
+  negate (P x) = mk $ prime - x
+  abs = id
+  signum (P x) = P $ signum x
+
+instance Bounded ModPrime where
+  minBound = 0
+  maxBound = -1
+
+instance Arbitrary ModPrime where
+  arbitrary = arbitraryBoundedEnum
+
 window :: Int
 window = 128
 
-powModPrime_naive :: Word64 -> Word64 -> Word64
-powModPrime_naive m n = fromInteger $ (toInteger m ^ toInteger n) `rem` toInteger prime
+size :: ModPrime
+size = 256
 
-powModPrime :: Word64 -> Word64 -> Word64
-powModPrime m 0 = 1
-powModPrime m 1 = m `rem` prime
-powModPrime m n = let (q,p) = n `quotRem` 2
-                      k = powModPrime m q
-                      f = if p == 0 then 1 else m
-                  in (k * k * f) `rem` prime
+droppedMultiplier :: ModPrime
+droppedMultiplier = size ^ (window - 1)
 
-prop_powModPrime_correct = forAll (choose (0, prime - 1)) $ \a ->
-                           forAll (choose (0, 50000)) $ \b ->
-                           powModPrime a b == powModPrime_naive a b
+roll1 :: ModPrime -> (ModPrime, ModPrime) -> ModPrime
+roll1 h (n,o) = size * (h - droppedMultiplier * o) + n
 
-roll1 :: Word64 -> (Word64, Word64) -> Word64
-roll1 h (n,o) = undefined
+roll :: [ModPrime] -> [ModPrime]
+roll xs = scanl roll1 0 (zip xs (replicate window 0 ++ xs))
+
+windowOrBigger :: Gen [ModPrime]
+windowOrBigger = sized $ \n -> do k <- choose (window, window + n)
+                                  vector k
+
+prop_roll = forAll windowOrBigger $ \x -> last (roll x) == last (roll (drop (length x - window) x))
