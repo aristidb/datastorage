537bb16dfd6436c4cb20e804f5ab2cf486975929
Aristid Breitkreuz <aristidb@gmail.com>
2013-11-16 20:29:36 +0100

    it can never be too generic

--- BlobStore.hs
+++ BlobStore.hs
@@ -15,16 +15,23 @@ data Store f = Store
     , load :: Address -> f (Maybe Object)
     }
 
+-- possible implementation of <|>
 orM :: Monad f => f (Maybe a) -> f (Maybe a) -> f (Maybe a)
 orM m n = do x <- m
              case x of
                Just _ -> return x
                Nothing -> n
 
-duplicated :: Monad f => (f () -> f () -> f ()) -> Store f -> Store f -> Store f
-duplicated (<>) a b = Store { cache = \o -> cache a o <> cache b o
-                            , store = \o -> store a o <> store b o
-                            , load = \i -> load a i `orM` load b i }
+type Rel a = a -> a -> a
+
+duplicated :: Monad f => Rel (f ()) -> Rel (f (Maybe Object)) ->
+                         Store f -> Store f -> Store f
+duplicated (<&>) (<|>) a b = Store { cache = \o -> cache a o <&> cache b o
+                            , store = \o -> store a o <&> store b o
+                            , load = \i -> load a i <|> load b i }
+
+duplicatedSerial :: Monad f => Store f -> Store f -> Store f
+duplicatedSerial = duplicated (>>) orM
 
 multi :: Monad f => (Address -> f (Store f)) -> Store f
 multi locate = Store { cache = xcache, store = xstore, load = xload }
